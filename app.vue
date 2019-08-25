f85c888f786f25e48f672c2bbaaec08fc933b56c
Boris Serebrov <serebrov@gmail.com>
2019-08-25 14:57:24 +0300

    Cut the example and update to v6.0.0

--- app.vue
+++ app.vue
@@ -1,42 +1,16 @@
 <template>
   <div>
     <div class="row">
-      <h1>Emoji Mart Vue 🏬</h1>
+      <h1>Emoji Mart Vue Fast Minimal Demo App 🏬</h1>
     </div>
     <div class="row">
-      <emoji emoji=":santa::skin-tone-3:" :size="32" />
-      <emoji emoji="santa" set="emojione" :size="32" />
-      <emoji :emoji="santaEmojiObject" :size="32" />
+      <emoji :data="index" emoji=":santa::skin-tone-3:" :size="32" />
+      <emoji :data="index" emoji="santa" set="emojione" :size="32" />
+      <emoji :data="index" :emoji="santaEmojiObject" :size="32" />
     </div>
 
     <div class="row">
-      <button @click="toggleVisible">Show / hide the picker</button>
-      <button @click="toggleVisible" v-html="smile"></button>
-    </div>
-    <div class="row">
-      <template
-        v-for="set in [
-          'native',
-          'apple',
-          'google',
-          'twitter',
-          'emojione',
-          'messenger',
-          'facebook'
-        ]"
-      >
-        <button
-          :key="set"
-          @click="activeSet = set"
-          :disabled="activeSet == set"
-        >
-          {{ set }}
-        </button>
-      </template>
-    </div>
-
-    <div v-if="isVisible" class="row">
-      <nimble-picker
+      <picker
         :data="index"
         :set="activeSet"
         :native="native"
@@ -49,131 +23,35 @@
 
     <div class="row-small">
       <iframe
-        src="https://ghbtns.com/github-btn.html?user=jm-david&repo=emoji-mart-vue&type=star&count=true"
+        src="https://ghbtns.com/github-btn.html?user=serebrov&repo=emoji-mart-vue&type=star&count=true"
         frameBorder="0"
         scrolling="0"
         width="90px"
         height="20px"
       ></iframe>
     </div>
-
-    <div class="row"></div>
-    <div class="row">
-      <div>Custom Search And Preview Templates Example</div>
-      <Picker :native="true">
-        <template slot="searchTemplate" slot-scope="slotProps">
-          <input
-            type="text"
-            :placeholder="slotProps.i18n.search"
-            v-on:input="slotProps.onSearch($event.target.value)"
-          />
-        </template>
-        <template slot="previewTemplate" slot-scope="slotProps">
-          <div class="emoji-mart-preview">
-            <div class="emoji-mart-preview-emoji">
-              <NimbleEmoji
-                :data="slotProps.data"
-                :emoji="slotProps.emoji ? slotProps.emoji : 'point_up'"
-                :native="slotProps.emojiProps.native"
-                :skin="slotProps.emojiProps.skin"
-                :set="slotProps.emojiProps.set"
-              />
-            </div>
-            <div class="emoji-mart-preview-data">
-              <div class="emoji-mart-preview-name">
-                {{
-                  slotProps.emoji
-                    ? "~" + slotProps.emoji.name + "~"
-                    : "Choose..."
-                }}
-              </div>
-              <div class="emoji-mart-preview-shortnames">
-                {{ slotProps.emoji ? slotProps.emoji.colons : "" }}
-              </div>
-            </div>
-          </div>
-        </template>
-      </Picker>
-    </div>
-
-    <div class="row">
-      <div>Filtered picker example</div>
-      <NimblePicker
-        :native="true"
-        emoji="flag-tf"
-        :emojiSize="18"
-        :data="indexFiltered"
-      />
-    </div>
   </div>
 </template>
 
 <script>
 import data from "emoji-mart-vue-fast/data/all.json";
-import {
-  Picker,
-  NimblePicker,
-  NimbleEmoji,
-  Emoji,
-  EmojiIndex
-} from "emoji-mart-vue-fast";
+import { Picker, Emoji, EmojiIndex } from "emoji-mart-vue-fast";
 import "emoji-mart-vue-fast/css/emoji-mart.css";
 
-const CUSTOM_EMOJIS = [
-  {
-    name: "Party Parrot",
-    short_names: ["parrot"],
-    keywords: ["party"],
-    imageUrl: "./images/parrot.gif"
-  },
-  {
-    name: "Octocat",
-    short_names: ["octocat"],
-    keywords: ["github"],
-    imageUrl: "https://github.githubassets.com/images/icons/emoji/octocat.png"
-  },
-  {
-    name: "Squirrel",
-    short_names: ["shipit", "squirrel"],
-    keywords: ["github"],
-    imageUrl: "https://github.githubassets.com/images/icons/emoji/shipit.png"
-  }
-];
-
-let index = new EmojiIndex(data, {
-  custom: CUSTOM_EMOJIS
-});
-
-let indexFiltered = new EmojiIndex(data, {
-  custom: CUSTOM_EMOJIS,
-  emojisToShowFilter: emoji => {
-    return emoji.short_names[0].match(/^flag.*/) !== null;
-  }
-});
+let index = new EmojiIndex(data);
 
 export default {
   data() {
     return {
       index: index,
-      indexFiltered: indexFiltered,
       activeSet: "native",
       emoji: "point_up",
-      title: "Pick your emoji…",
-      custom: CUSTOM_EMOJIS,
-      isVisible: true
+      title: "Pick your emoji…"
     };
   },
   computed: {
     native() {
       return this.activeSet == "native";
-    },
-    smile() {
-      // Static emoji example
-      let emoji = index.findEmoji(":smile:");
-      // Note, that position in the emoji sheet is calculated by
-      // `emoji` object
-      let style = `background-position: ${emoji.getPosition()}; background-image: url(https://unpkg.com/emoji-datasource-emojione@4.0.4/img/emojione/sheets-256/64.png); width: 24px; height: 24px; display: inline-block; background-size: 5200%`;
-      return `<div class='emoji' style="${style}"></div>`;
     },
     santaEmojiObject() {
       return index.findEmoji(":santa:");
@@ -186,9 +64,7 @@ export default {
   },
   components: {
     Picker,
-    NimblePicker,
-    Emoji,
-    NimbleEmoji
+    Emoji
   }
 };
 </script>
